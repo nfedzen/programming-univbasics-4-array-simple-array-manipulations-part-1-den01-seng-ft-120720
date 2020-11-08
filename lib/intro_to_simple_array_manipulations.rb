1337c1d9d4457cc5ddb3b9dbb5027ed1caad8cda
Nathan Fedzen <nfedzen@hotmail.com>
2020-11-08 05:48:06 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,23 @@
+def using_push(array, word)
+  array.push(word)
+end
+
+def using_unshift(array, word)
+  array.unshift(word)
+end
+
+def using_pop(array)
+  array.pop
+end
+
+def pop_with_args(array)
+  array.pop(2)
+end
+
+def using_shift(array)
+  array.shift 
+end
+
+def shift_with_args(array)
+  array.shift(2)
+end
